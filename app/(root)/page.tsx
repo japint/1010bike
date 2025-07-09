5ba9fee7214989398964654c3ba804c333040d4c
meistergpc <jap1centeno@gmail.com>
2025-07-10 00:24:41 +0200

    Theme mode toggle

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+export const metadata = {
+  title: "Home",
+};
+
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Welcome to Next.js with shadcn/ui</h1>
+      <Button>Button</Button>
+    </main>
+  );
+};
+
+export default HomePage;
