79822d8876e1a25bd9d0e521799afacc1c152a34
meistergpc <jap1centeno@gmail.com>
2025-07-11 00:18:24 +0200

    add zod validators and type interference

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import ProductPrice from "./product-price";
+import { Product } from "@/types";
 
-const ProductCard = ({ product }: { product: any }) => {
+const ProductCard = ({ product }: { product: Product }) => {
   if (!product || !product.slug || !product.images?.[0] || !product.name) {
     // Optionally return fallback UI or nothing
     return null;
