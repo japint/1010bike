c34137b249372f64556dd90948435bbfa397c070
meistergpc <jap1centeno@gmail.com>
2025-07-13 21:59:19 +0200

    update

--- auth.ts
+++ auth.ts
@@ -10,52 +10,52 @@ export const config = {
     error: "/sign-in",
   },
   session: {
-    strategy: "jwt",
+    strategy: "jwt" as const,
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  adapter: PrismaAdapter(prisma),   
-    providers: [
-        CredentialsProvider({
-            email: {
-                type: "email",
-            },
-            password: {
-                type: "password",
-            },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          type: "email",
         },
-        async authorize(credentials) => {
-            if (credentials == null) return null;
+        password: {
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
 
-            // find user in database
-            const user = await prisma.user.findFirst({
-                where: {
-                    email: credentials.email as string,
-                }
-            });
+        // find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
 
-            // check if user exists and if password matches
-            if (user && user.password) {
-                const isMatch = compareSync(credential.password, user.password);
+        // check if user exists and if password matches
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
 
-                // if password is correct, return user object
-                if (isMatch) {
-                    return {
-                        id: user.id,
-                        name: user.name,
-                        email: user.email,
-                        role: user.role,
-                    }
-                }
-            }
-            // if user not found or password does not match, return null
-            return null;
-                return {
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                };
-            }
-        })],
-    }
+          // if password is correct, return user object
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        // if user not found or password does not match, return null
+        return null;
+      },
+    }),
+  ],
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
