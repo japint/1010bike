19bfc2a78e0b00d3a7085d4c11adeec5b9335784
meistergpc <jap1centeno@gmail.com>
2025-07-16 00:52:09 +0200

    Add cart price calculation and round2 utility function
    - Implemented calcPrice function to calculate item prices, shipping, tax, and total.
    - Added round2 function to round numbers to two decimal places.
    - Updated addItemToCart to create a new cart if it doesn't exist and revalidate product page.

--- lib/utils.ts
+++ lib/utils.ts
@@ -58,3 +58,14 @@ export function formatError(error: unknown): string {
   // If error is not an object, try to convert to string
   return String(error) || "An unexpected error occurred";
 }
+
+// round number to 2 decimal places
+export function round2(value: number | string) {
+  if (typeof value === "number") {
+    return Math.round((value + Number.EPSILON) * 100) / 100;
+  } else if (typeof value === "string") {
+    return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
+  } else {
+    throw new Error("Value is not a number or string");
+  }
+}
