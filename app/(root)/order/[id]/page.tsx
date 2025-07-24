59c73cae2acad807586771202c3dda2ee79b78f7
meistergpc <jap1centeno@gmail.com>
2025-07-24 17:06:28 +0200

    Enhance OrderDetailsPage: add order ID formatting, display order date, status, and total price with currency formatting; improve delivery status indication

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { getOrderById } from "@/lib/actions/order.actions";
 import { notFound } from "next/navigation";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
 // import { ShippingAddress } from "@/types";
 
 export const metadata: Metadata = {
@@ -15,10 +16,27 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
     notFound();
   }
 
+  const { dateTime: orderDate } = formatDateTime(order.createdAt);
+  const deliveredDate = order.deliveredAt
+    ? formatDateTime(order.deliveredAt).dateTime
+    : "Not delivered yet";
+
   return (
     <div className="max-w-4xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-4">Order Details</h1>
+      <div className="mb-8">
+        <h1 className="text-2xl font-bold">Order #{formatId(order.id)}</h1>
+        <p className="text-muted-foreground">Placed on {orderDate}</p>
+      </div>
       <div className="bg-gray-100 p-4 rounded">
+        <span>Status:</span>
+        <span className="font-medium">
+          {order.isPaid ? (
+            <span className="text-green-600">Paid</span>
+          ) : (
+            <span className="text-yellow-600">Pending</span>
+          )}
+        </span>
         <p>
           <strong>Order ID:</strong> {id}
         </p>
@@ -26,7 +44,15 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
           <strong>ID Type:</strong> {typeof id}
         </p>
         <p>
-          <strong>Total Price:</strong> {order.totalPrice.toString()}
+          <strong>Total Price:</strong>{" "}
+          {formatCurrency(Number(order.totalPrice))}
+        </p>
+        <p className="text-muted-foreground mt-1">
+          {order.isDelivered ? (
+            <span className="text-green-600">Delivered on {deliveredDate}</span>
+          ) : (
+            <span className="text-yellow-600">Processing</span>
+          )}
         </p>
       </div>
     </div>
