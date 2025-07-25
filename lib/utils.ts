e408c5a8cbf153c29c0b64c508c57a4f7765458c
meistergpc <jap1centeno@gmail.com>
2025-07-25 12:03:39 +0200

    Refactor utils: remove unused query-string import and delete formUrlQuery function for cleaner code

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import qs from "query-string";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -139,28 +138,3 @@ export const formatDateTime = (dateString: Date) => {
     timeOnly: formattedTime,
   };
 };
-
-// Form the pagination links
-export function formUrlQuery({
-  params,
-  key,
-  value,
-}: {
-  params: string;
-  key: string;
-  value: string | null;
-}) {
-  const query = qs.parse(params);
-
-  query[key] = value;
-
-  return qs.stringifyUrl(
-    {
-      url: window.location.pathname,
-      query,
-    },
-    {
-      skipNull: true,
-    }
-  );
-}
