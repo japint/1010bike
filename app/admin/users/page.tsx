468655d9c7bf8b8d78ef5ab9a7af6c7da87d68fb
meistergpc <jap1centeno@gmail.com>
2025-09-04 18:59:47 +0200

    Add AdminUsersPage for managing users with pagination and role display

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,81 @@
+import { Metadata } from "next";
+import { getAllUsers } from "@/lib/actions/user.action";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatId } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Pagination from "@/components/shared/pagination";
+import { Badge } from "@/components/ui/badge";
+
+export const metadata: Metadata = {
+  title: "Admin - Users",
+  description: "Admin Users Page",
+};
+
+const AdminUsersPage = async (props: {
+  searchParams: Promise<{
+    page: string;
+  }>;
+}) => {
+  const { page = "1" } = await props.searchParams;
+
+  const users = await getAllUsers({ page: Number(page) });
+  console.log(users);
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Users</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>NAME</TableHead>
+              <TableHead>EMAIL</TableHead>
+              <TableHead>ROLE</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {(users?.data ?? []).length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={5}>No users found.</TableCell>
+              </TableRow>
+            ) : (
+              (users?.data ?? []).map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell>{formatId(user.id)}</TableCell>
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                  <TableCell>
+                    {user.role === "user" ? (
+                      <Badge variant="secondary">User</Badge>
+                    ) : (
+                      <Badge variant="default">Admin</Badge>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    <Button asChild variant="outline" size="sm">
+                      <Link href={`/admin/users/${user.id}`}>Edit</Link>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+        {users.totalPages > 1 && (
+          <Pagination page={Number(page) || 1} totalPages={users?.totalPages} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AdminUsersPage;
